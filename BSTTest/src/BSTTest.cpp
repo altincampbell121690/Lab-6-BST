9f497036c49299017f65a16ea9121aad6a09f94a
Nitla <31260015+altincampbell121690@users.noreply.github.com>
2017-11-02 10:15:19 -0700

    Updated test file
    not finished

--- BSTTest/src/BSTTest.cpp
+++ BSTTest/src/BSTTest.cpp
@@ -12,16 +12,40 @@
 using namespace std;
 
 int main() {
-
-	cout << "!!!*Hello World*!!!" << endl; // prints !!!Hello World!!!
 	BST<int>tree1;
-	tree1.insert(5);
-	tree1.insert(2);
-	tree1.insert(6);
-	tree1.insert(1);
-	tree1.insert(0);
-	tree1.inOrderPrint(cout);
+	//BST<string>tree2;
+//**
+	cout << "!!!*Hello World*!!!" << endl; // prints !!!Hello World!!!
+	//testing insert
+	tree1.insert(40);
+	tree1.insert(25);
+	tree1.insert(78);
+	//tree1.insert(3);
+	tree1.insert(10);
+	tree1.insert(32);
+	//testing Inorder print
+	//tree1.inOrderPrint(cout);
+	//testing postrder print
+	tree1.postOrderPrint(cout);
+///**/
 
-	cout << "!!!*Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << tree1.getSize() << endl;
+    //test is empty
+	cout << "should print: not empty\n";
+	if(tree1.isEmpty())
+		cout << "it is empty\n";
+	else
+		cout <<"not empty\n";
+	cout << "should print: Found\n";
+	if(tree1.search(32))
+		cout << "FOUND: 32\n";
+	else
+		cout <<"not HERE\n";
+	cout << "should print: not HERE\n";
+	if(tree1.search(15))
+			cout << "FOUND\n";
+		else
+			cout <<"not HERE\n";
+	cout << "!!!*Hello World**~!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
